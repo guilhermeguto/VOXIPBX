de2bc9ab28f6d0ffd739e0c0a8e72f70a37c1325
Emerson Reis <eluizbr@gmail.com>
2014-03-21 14:14:16 -0300

    Update install-g729.sh

--- install/install-g729.sh
+++ install/install-g729.sh
@@ -6,13 +6,13 @@ BRANCH='devel'
 
 	if [ ! -d "/etc/asterisk" ]; then
 		
+		func_install_asterisk
 		func_install_g729
 		bash install-asterisk.sh
                 ExitFinish=1
 
 		
 	else
-		func_install_asterisk
 		func_install_g729
 		bash install-asterisk.sh
                 ExitFinish=1
