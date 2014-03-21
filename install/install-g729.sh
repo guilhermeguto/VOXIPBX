d1d6a4aecad1fbb30e8d94c1558742790e5f90b0
Emerson Reis <eluizbr@gmail.com>
2014-03-21 14:09:57 -0300

    Update install-g729.sh

--- install/install-g729.sh
+++ install/install-g729.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
+source funcoes.sh
+BRANCH='devel'
 
-cpu=`getconf LONG_BIT`
+# Checar asterisk
 
-if echo $cpu | grep -i "32" > /dev/null ; then
-	echo "32"
-	cd /var/www/ipbx/
-	git pull origin master
-	cd /usr/lib/asterisk/modules/
-	wget -c http://asterisk.hosting.lv/bin/codec_g729-ast18-gcc4-glibc-pentium.so
-	mv codec_g729-ast18-gcc4-glibc-pentium.so codec_g729.so
-	chmod 755 /usr/lib/asterisk/modules/codec_g729.so
-	asterisk -x "core restart now"
-else
-	echo "64"
-	cd /var/www/ipbx/
-	git pull origin master
+	if [ ! -d "/etc/asterisk" ]; then
+		
+		func_install_g729
+		bash install-asterisk.sh
+                ExitFinish=1
 
-	cd /usr/lib/asterisk/modules/
-	wget -c http://asterisk.hosting.lv/bin/codec_g729-ast18-icc-glibc-x86_64-pentium4.so
-	mv codec_g729-ast18-icc-glibc-x86_64-pentium4.so codec_g729.so
-	chmod 755 /usr/lib/asterisk/modules/codec_g729.so
-	asterisk -x "core restart now"
+		
+	else
+		func_install_asterisk
+		func_install_g729
+		bash install-asterisk.sh
+                ExitFinish=1
 
-fi
-
+	fi
+		
+		
 # Fim seta CPU
 
 
