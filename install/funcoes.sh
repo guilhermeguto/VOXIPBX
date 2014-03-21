583e89b5b621dd61a2059639a4e0f3fcdfb23e0e
Emerson Reis <eluizbr@gmail.com>
2014-03-21 14:29:07 -0300

    Update funcoes.sh

--- install/funcoes.sh
+++ install/funcoes.sh
@@ -142,11 +142,9 @@ func_install_g729 () {
 
 
 				cpu=`getconf LONG_BIT`
-
 		if echo $cpu | grep -i "32" > /dev/null ; then
 				echo "32"
-				cd /var/www/ipbx/
-				git pull origin $BRANCH
+				cd /usr/src/
 				cd /usr/lib/asterisk/modules/
 				wget -c http://asterisk.hosting.lv/bin/codec_g729-ast18-gcc4-glibc-pentium.so
 				mv codec_g729-ast18-gcc4-glibc-pentium.so codec_g729.so
@@ -154,9 +152,7 @@ func_install_g729 () {
 				asterisk -x "core restart now"
 		else
 				echo "64"
-				cd /var/www/ipbx/
-				git pull origin $BRANCH
-
+				cd /usr/src/
 				cd /usr/lib/asterisk/modules/
 				wget -c http://asterisk.hosting.lv/bin/codec_g729-ast18-icc-glibc-x86_64-pentium4.so
 				mv codec_g729-ast18-icc-glibc-x86_64-pentium4.so codec_g729.so
