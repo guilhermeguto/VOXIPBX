bd4e8a9ae3f8fb9d5cbf56caac947ac4d44c4135
Emerson Reis <eluizbr@gmail.com>
2014-03-30 14:32:54 -0300

    Update install-pabx.sh

--- install/install-pabx.sh
+++ install/install-pabx.sh
@@ -13,7 +13,7 @@ apt-get -y install lsb-release
 # Identify Linux Distribution type
 func_identify_os() {
     if [ -f /etc/debian_version ] ; then
-        DIST='DEBIAN'
+        DIST='UBUNTU'
         
         if [ "$(lsb_release -cs)" != "precise" ] ; then
             	echo "A instalação funciona apenas no Ubuntu LTS 12.04"
