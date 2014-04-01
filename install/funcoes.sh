4df387f7c499ec59241bd700f7fadacf996b6ce5
Emerson Reis <eluizbr@gmail.com>
2014-04-01 11:40:16 -0300

    Update funcoes.sh

--- install/funcoes.sh
+++ install/funcoes.sh
@@ -94,8 +94,8 @@ func_install_dahdi ()  {
                         	wget -c http://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/dahdi-linux-complete-2.9.1+2.9.1.tar.gz
                         	
                         	# Instalando DAHDI
-                        	tar xvfz dahdi-linux-complete-2.9.0+2.9.1.tar.gz
-                        	ln -s dahdi-linux-complete-2.9.0+2.9.1/ dahdi
+                        	tar xvfz dahdi-linux-complete-2.9.1+2.9.1.tar.gz
+                        	ln -s dahdi-linux-complete-2.9.1+2.9.1/ dahdi
 				cd dahdi
 				
 				# Complinado o OSLEC
